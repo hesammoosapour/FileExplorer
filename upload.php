0645ff2e9f8a0ddf258b210d77e68a156825d93b
unknown <Administrator@PC0011.ipinbar.ir>
2021-12-29 09:49:27 +0330

    showing only user's own folder content

--- upload.php
+++ upload.php
@@ -10,34 +10,21 @@
     <br><br>
     <a href="logout.php">Click here</a> to Logout.
 
-    <div class="col-4">
-        <form action="myfunctions.php" method="post">
-            <p class="mt-3"><label for="newfilename"></label></p>
-            <input type="text" name="newfilename" id="newfilename" value="" placeholder="Enter New File Name" class="form-control">
-            <br>
-            <input type="submit" class="btn btn-success" value="Create New File" name="submit_create_new_file">
-        </form>
-    </div>
+<!--    <div class="col-4">-->
+<!--        <form action="myfunctions.php" method="post">-->
+<!--            <p class="mt-3"><label for="newfilename"></label></p>-->
+<!--            <input type="text" name="newfilename" id="newfilename" value="" placeholder="Enter New File Name" class="form-control">-->
+<!--            <br>-->
+<!--            <input type="submit" class="btn btn-success" value="Create New File" name="submit_create_new_file">-->
+<!--        </form>-->
+<!--    </div>-->
     <br>
     <?php
     //$content = scandir('.');
     //print_r($content);
 
-
-    foreach (glob('./*', GLOB_ONLYDIR) as $dir) {
-        $dirname = basename($dir);
-
-        $dir = substr($dir, 2);
-        ?>
-        <br>
-        <a href="?p=<?= $dir ?>"  >
-            <i class="fa fa-folder-o"></i> <?= $dir ?>
-        </a>
-
-    <?php }  ?>
-
-    <?php
-    $dirPath = './';
+//    $dir = scandir('./'.$_SESSION['Username'].'/');
+    $dirPath = './'.$_SESSION['Username'];
 
     $dir = scandir($dirPath);
 
@@ -45,9 +32,34 @@
     {
         if(is_dir($dirPath. '/' . $item))
         {
+            $directories[] = $dir[$index];
             unset($dir[$index]);
         }
     }
+
+
+    unset($directories[0], $directories[1]);
+
+    foreach ( $directories as $directory){
+
+//    foreach (glob('./'.$_SESSION['Username'].'/', GLOB_ONLYDIR) as $dir) {
+//        $dirname = basename($dir);
+
+//        $dir = substr($dir, 2);
+        ?>
+        <br>
+        <a href="?p=<?= $directory ?>"  >
+            <i class="fa fa-folder-o"></i> <?= $directory ?>
+        </a>
+<!--        <br>-->
+<!--        <a href="?p=--><?//= $dir ?><!--"  >-->
+<!--            <i class="fa fa-folder-o"></i> --><?//= $dir ?>
+<!--        </a>-->
+
+    <?php }  ?>
+
+    <?php
+
 
     $files = array_values($dir);
 
